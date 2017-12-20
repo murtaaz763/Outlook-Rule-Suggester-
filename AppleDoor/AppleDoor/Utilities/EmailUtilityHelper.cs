5c26fa9c9938b2f91a90cbbf3ee7ac2bd26e5ffa
Murtaaz763 <murtaaz763@gmail.com>
2017-12-20 21:27:01 +0530

    Committing EmailUtilityHelper, and transferred two methods

---
+++ AppleDoor/AppleDoor/Utilities/EmailUtilityHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleDoor.Utilities
+{
+    class EmailUtilityHelper
+    {
+        private string encryptMailData(string mailItem)
+        {
+            string output = "";
+            try
+            {
+                char[] readChar = mailItem.ToCharArray();
+
+                for (int i = 0; i < readChar.Length; i++)
+                {
+                    if (!readChar[i].Equals('\t'))
+                    {
+                        int no = Convert.ToInt32(readChar[i]) + 10;
+                        string r = Convert.ToChar(no).ToString();
+                        output += r;
+                    }
+                    else
+                    {
+                        int no = Convert.ToInt32(readChar[i]);
+                        string r = Convert.ToChar(no).ToString();
+                        output += r;
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return output;
+
+        }
+
+        private string decryptMailData(String mailItem)
+        {
+            //decryption  
+            string output = "";
+            char[] readChar = mailItem.ToCharArray();
+            for (int i = 0; i < readChar.Length; i++)
+            {
+                int no = Convert.ToInt32(readChar[i]) - 10;
+                string r = Convert.ToChar(no).ToString();
+                output += r;
+            }
+            return output;
+        }
+
+    }
+}
